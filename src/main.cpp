8938c3e5fe6e5223bc4f540d25bfc9609c5a05d5
Anastasiya <nastya.pak2206@gmail.com>
2021-06-02 09:20:19 +0500

    main

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,6 @@
 
 int main(int argc, char** argv) {
 	for (int i = 1, flag = 0; i < argc; i++) {
-		if (!DEBUG && strcmp(argv[i], "--verbose") == 0) {
-			DEBUG = true;
-		}
 		if (strstr(argv[i], ".txt") != nullptr) {
 			if (strcmp(argv[i - 1], "<") == 0 || strcmp(argv[i - 1], "<<") == 0) 
 				INTERACTIVE = false;
@@ -45,36 +42,6 @@ int main(int argc, char** argv) {
 			}
 		}
 		init_functions(infixlines);
-		if (DEBUG) {
-			for (int i = 0; i < infixlines.size(); ++i) {
-				std::cout << std::endl << i + 1 << ": size of infix = " << infixlines[i].size() << std::endl;
-				for (auto &debug: infixlines[i]) {
-					if (debug == nullptr) {
-						continue;
-					}
-					if (Number *ptr = dynamic_cast<Number *>(debug)) {
-						((Number *)debug) -> print();
-						continue;
-					}
-					if (Variable *ptr = dynamic_cast<Variable *>(debug)) {
-						((Variable *)debug) -> print();
-						continue;
-					}
-					if (Function *ptr = dynamic_cast<Function *>(debug)) {
-						((Function *)debug) -> print();
-						continue;
-					}
-					if (Goto *ptr = dynamic_cast<Goto *>(debug)) {
-						((Goto *)debug) -> print();
-					} else {
-						((Oper *)debug) -> print();
-					}
-				}
-				std::cout << "\n----------------------------------\n";
-			}
-			std::cout << "##################################\n";
-			std::cout << "##################################\n";
-		}
 		for (const auto &infix: infixlines) {
 			try {
 				postfixlines.push_back(build_postfix(infix));
@@ -82,36 +49,6 @@ int main(int argc, char** argv) {
 				std::cerr << "problems in " << postfixlines.size() + 1 << " string:\n";
 				throw (e);	
 			}
-		}
-		if (DEBUG) {
-			for (int i = 0; i < postfixlines.size(); ++i) {
-				std::cout << std::endl << i + 1 << ": size of postfix = " << postfixlines[i].size() << std::endl;
-				for (auto &debug: postfixlines[i]) {
-					if (debug == nullptr) {
-						continue;
-					}
-					if (dynamic_cast<Number *>(debug)) {
-						((Number *)debug) -> print();
-						continue;
-					}
-					if (dynamic_cast<Variable *>(debug)) {
-						((Variable *)debug) -> print();
-						continue;
-					}
-					if (dynamic_cast<Function *>(debug)) {
-						((Function *)debug) -> print();
-						continue;
-					}
-					if (dynamic_cast<Goto *>(debug)) {
-						((Goto *)debug) -> print();
-					} else {
-						((Oper *)debug) -> print();
-					}
-				}
-				std::cout << "\n----------------------------------\n";
-			}
-			std::cout << "##################################\n";
-			std::cout << "##################################\n";
 		}
 		Number *res = nullptr;
 		if (Function::ftable.find("main") == Function::ftable.end()) {
@@ -122,12 +59,6 @@ int main(int argc, char** argv) {
 		while (0 <= row && row < (int)postfixlines.size()) {
 			try {
 				row = evaluate_postfix(postfixlines, row, &main, &res);
-				if (DEBUG || INTERACTIVE) {	
-					if (res != nullptr) {
-						std::cout << ">>>>>>" <<(res -> get_value());
-						delete res;
-					}
-				}
 				std::cout << std::endl;
 			} catch (ERRORS e) {
 				std::cerr << "problems in " << row + 1 << " string:\n";
